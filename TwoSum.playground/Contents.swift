c80be92cb387efccd18787b93313ae9bde36497c
StPayne55 <spayne@locqus.com>
2017-06-16 16:46:31 -0400

    Will add task description to TwoSum

--- TwoSum.playground/Contents.swift
+++ TwoSum.playground/Contents.swift
@@ -1,8 +1,20 @@
-//: Playground - noun: a place where people can play
+/*
+ Task:
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    Example:
+    Given nums = [2, 7, 11, 15], target = 9,
+
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+*/
+
 
 import UIKit
 
-/// This appears to be as optimized as possible. Runs faster than 95% of solutions.
+/// This appears to be as optimized as possible. Runs with O(n) time complexity where n is equal to the number of integers in the set at the worst.
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     var dict: [Int: Int] = [:]
 
